d67927c1b8a01d72b48b9494d773b2aa817b2a91
emil7f <emilfigzal@gmail.com>
2021-03-28 18:46:18 +0200

    currency mini-game

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Currency, CurrencyServiceService} from '../services/currency-service.service';
 
 @Component({
   selector: 'app-game',
@@ -7,14 +8,29 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GameComponent implements OnInit {
   messageForUser: string;
+  currency: Currency;
+  result: string;
 
-  constructor() {
+  constructor(private currencyService: CurrencyServiceService) {
   }
 
   ngOnInit(): void {
+    this.currencyService.getCurrency().subscribe(value => {
+      this.currency = value;
+    });
   }
 
   sayHello(value: string) {
     this.messageForUser = 'Hello ' + value;
+  }
+
+  check(value: number) {
+    if (value > this.currency.rates.PLN) {
+      this.result = 'Your value is too high!';
+    } else if (value < this.currency.rates.PLN) {
+      this.result = 'Your value is too small!';
+    } else {
+      this.result = 'You did it!';
+    }
   }
 }
